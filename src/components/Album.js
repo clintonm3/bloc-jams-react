8c59ce7c25d0c7ccbe474aba206b0cfb27974667
Clinton Murphy <clintonmurphy@Clintons-MacBook-Pro.local>
2018-05-24 21:53:50 -0600

    Assignment 7 completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
++import albumData from './styles/album.css';
 
 class Album extends Component {
   constructor(props) {
@@ -64,7 +65,13 @@ class Album extends Component {
           <tbody>
           {this.state.album.songs.map((song, index) =>
        		   <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
-                <td className="song-number">{index + 1}</td>
+                <td className="song-actions">
+                  <button>
+                    <span className="song-number">{index + 1}</span>
+                    <span className="ion-play"></span>
+                    <span className="ion-pause"></span>
+                  </button>
+                </td>
                 <td className="song-title">{song.title}</td>
                 <td className="song-duration">{song.duration}</td>
              </tr>
